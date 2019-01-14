1a95015b1237d181e2b8dfcbd510892c5b5c3eec
Nat-Reid <nathanielreid@gmail.com>
2019-01-14 13:29:27 -0800

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  #tries to add data at deepest level, then scopes out if item doesn't exist at that level
+  pigeon_list= Hash.new
+  data.each do |trait, options|
+    options.each do |option, birds|
+      birds.each do |bird|
+        if pigeon_list.include?(bird)
+          if pigeon_list[bird].include?(trait)
+            pigeon_list[bird][trait] << option.to_s
+          else
+            pigeon_list[bird][trait] = [option.to_s]
+          end
+        else
+          pigeon_list[bird] = {trait => [option.to_s]}
+        end
+      end
+    end
+  end
+  pigeon_list
 end
